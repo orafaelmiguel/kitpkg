a461de6f7c4c61456fccee9939292c09154255f3
Rafael Miguel <rafaelmiguel0202@gmail.com>
2026-04-14 18:46:00 -0300

    refactor autocomplete to works with args and pipes

--- internal/shell/autocomplete.go
+++ internal/shell/autocomplete.go
@@ -1,6 +1,8 @@
 package shell
 
 import (
+	"os"
+	"path/filepath"
 	"strings"
 
 	"kitpkg/internal/commands"
@@ -13,10 +15,10 @@ func HandleTab(input string, commandMap map[string]commands.Command) string {
 		return input
 	}
 
-	if len(args) == 1 {
+	if len(args) == 1 && !strings.Contains(input, " ") {
 		prefix := args[0]
 
-		matches := []string{}
+		var matches []string
 		for name := range commandMap {
 			if strings.HasPrefix(name, prefix) {
 				matches = append(matches, name)
@@ -28,6 +30,44 @@ func HandleTab(input string, commandMap map[string]commands.Command) string {
 		}
 
 		return input
+	}
+
+	lastArg := args[len(args)-1]
+
+	dir := "."
+	prefix := lastArg
+
+	if strings.Contains(lastArg, "/") {
+		dir = filepath.Dir(lastArg)
+		prefix = filepath.Base(lastArg)
+	}
+
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return input
+	}
+
+	var matches []string
+
+	for _, entry := range entries {
+		name := entry.Name()
+
+		if strings.HasPrefix(strings.ToLower(name), strings.ToLower(prefix)) {
+			if entry.IsDir() {
+				name += "/"
+			}
+
+			if dir != "." {
+				name = filepath.Join(dir, name)
+			}
+
+			matches = append(matches, name)
+		}
+	}
+
+	if len(matches) == 1 {
+		args[len(args)-1] = matches[0]
+		return strings.Join(args, " ")
 	}
 
 	return input
